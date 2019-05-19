103ab4303c57fc0e7fa4d25f4a44b597338c9c35
Yuliya <sun51188@gmail.com>
2019-05-19 13:07:06 +0300

    Add friends ids filter via regex

--- instagram_bot.py
+++ instagram_bot.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import instabot
 from dotenv import load_dotenv
+import re
 
 
 # def parse_arguments():
@@ -49,6 +50,23 @@ def get_comments_of_post(login, password, post_url):
     # comments = bot.get_media_comments_all(post_id)
     comments = bot.get_media_comments(post_id)        # ONLY FOR DEBUGGING!
     return comments
+
+def get_friend_ids(comment):
+    regex = re.compile("(?:^|[^\\w])(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_])){0,1})")
+    matchArray = regex.findall(comment)
+    return matchArray
+
+
+def filter_comments_with_link_to_friend(comments):
+    comments_with_links = []
+    for index, comment in enumerate(comments):
+        friends = get_friend_ids(comment["text"])
+        print(f'Friends: {friends} of comment#{index}')
+        if len(friends) > 0:
+            comments_with_links.append({'comment': comment, 'friends':friends})
+    return comments_with_links
+
+
 def main():
     load_dotenv()
     inst_login = os.getenv("INST_LOGIN")
@@ -61,6 +79,8 @@ def main():
     # print(test_instabot(inst_login, inst_password))
 
     comments = get_comments_of_post(inst_login, inst_password, post_url)
+    filtered_comments = filter_comments_with_link_to_friend(comments)
+ 
 
 if __name__ == '__main__':
     main()
