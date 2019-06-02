4be90d2c6f2ddfb754100a9c188b2af4c0c45e13
Yuliya <sun51188@gmail.com>
2019-06-02 17:57:09 +0300

    Add user name validator
    def valid_user_names(comments)

--- instagram_bot.py
+++ instagram_bot.py
@@ -52,7 +52,8 @@ def get_comments_of_post(login, password, post_url):
     return comments
 
 def get_friend_ids(comment):
-    regex = re.compile("(?:^|[^\\w])(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_])){0,1})")
+    regex = re.compile("(?:^|[^\\w])(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|"
+        "(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_])){0,1})")
     matchArray = regex.findall(comment)
     return matchArray
 
@@ -61,11 +62,25 @@ def filter_comments_with_link_to_friend(comments):
     comments_with_links = []
     for index, comment in enumerate(comments):
         friends = get_friend_ids(comment["text"])
-        print(f'Friends: {friends} of comment#{index}')
+        # print(f'Friends: {friends} of comment#{index}')
         if len(friends) > 0:
             comments_with_links.append({'comment': comment, 'friends':friends})
     return comments_with_links
 
+
+def valid_user_names(comments):
+    bot = instabot.Bot()
+    bot.login(username=login, password=password)
+    valid_comment = []
+    for comment in comments:
+        friends_id = [bot.get_user_id_from_username(username) for username in comment["friends"]]
+        if not friends_id is None:
+            valid_comment.append(comment)
+        print(f'ID друзей: {friends_id}')
+
+    # users_exist = [bool(user_id) for user_id in user_ids]
+    # print(f'Логическая сумма: {sum(users_exist)}')
+    return 
 
 def main():
     load_dotenv()
@@ -77,10 +92,33 @@ def main():
     #     exit("The specified directory '{0}' doesn't exist".format(args.folder))
     # publish_to_instagramm(inst_login, inst_password, args.folder)
     # print(test_instabot(inst_login, inst_password))
-
+    
     comments = get_comments_of_post(inst_login, inst_password, post_url)
     filtered_comments = filter_comments_with_link_to_friend(comments)
- 
+    participants = [comment["comment"] for comment in filtered_comments if is_users_exist(inst_login, inst_password, comment['friends'])]
+    print(f'Участники: {participants}')
+
+
 
 if __name__ == '__main__':
     main()
+
+"""
+Friends: ['tamella_1', 'solnechny.chelovechek'] of comment#0
+Friends: ['marfi_777', 'larisa_altuxova'] of comment#1
+Friends: ['ko.m.a.r.i.k', 'gif_glm'] of comment#2
+Friends: ['polioli___', 'st.777_'] of comment#3
+Friends: ['_vassilek', 'galimjanovakylia'] of comment#4
+Friends: ['zameerka', 'happy__olga'] of comment#5
+Friends: ['grigorieva88', 'mariiakuzovleva'] of comment#6
+Friends: ['moskaleva35', 'marinagrigoreva1501'] of comment#7
+Friends: ['daryaostik', 'mariakuzovleva86'] of comment#8
+Friends: ['dglush', 'la.risa7158'] of comment#9
+Friends: ['grigore3861', 'mariakuzovleva6'] of comment#10
+Friends: ['tolstik_kate_13', '_ann_boychuk_'] of comment#11
+Friends: ['lovely_sofi_', '_m.lizzie._'] of comment#12
+Friends: ['beautybar.rus'] of comment#13
+Friends: ['avgkop'] of comment#14
+Friends: ['avgkop'] of comment#15
+Friends: ['ekaterinavecher'] of comment#1
+"""
